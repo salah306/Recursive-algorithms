7b3dd617c526de8bea8798a6cd33544388ad4eb8
salah306 <mohammed.salah.ahmed@gmail.com>
2017-10-06 22:17:23 +0200

    inti

---
+++ RecursiveDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace RecursiveDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tests.AreEqual(Power.CopmutePower(3, 0), 1));
+            Console.ReadKey();
+        }
+
+    }
+
+
+    static class Tests
+    {
+        public static string AreEqual(double expected, double actual)
+        {
+            return expected == actual ? string.Format(" Test Passed- {0} raised to power {1} is {2}" , expected , actual , Math.Pow(expected , actual)) 
+                : string.Format("Test Failed - Expected {0} - Actual {1}", Math.Pow(expected, actual), Power.CopmutePower(expected , actual));  
+        }
+    }
+
+    public  static class Power
+    {
+
+
+        public static double CopmutePower(double x , double n)
+        {
+            // base case
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            // recursive case: n is negative 
+            else if (n < 0)
+            {
+                return 1 / CopmutePower(x, -n);
+            }
+            // recursive case: n is odd
+            else if (IsOdd(n))
+            {
+                return x * CopmutePower(x, n - 1);
+            }
+            // recursive case: n is even
+            else if (IsEven(n))
+            {
+                var y = CopmutePower(x, n / 2);
+                return y * y;
+            }
+            else
+            {
+                return 0;
+            }
+
+           
+        }
+        
+        static bool IsEven(double n)
+        {
+            return n % 2 == 0;
+        }
+
+        static bool IsOdd(double n)
+        {
+            return !IsEven(n);
+        }
+    }
+}
---
+++ RecursiveDemoTests/PowerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RecursiveDemo.Tests
+{
+    [TestClass()]
+    public class PowerTests
+    {
+        [TestMethod()]
+        public void CopmutePower_3_raised_to_power_0()
+        {
+            Assert.AreEqual( Math.Pow(3 , 0), Power.CopmutePower(3, 0));
+       
+
+        }
+        [TestMethod()]
+        public void CopmutePower_3_raised_to_power_1()
+        {
+            Assert.AreEqual(Math.Pow(3, 1), Power.CopmutePower(3, 1));
+        }
+
+        [TestMethod()]
+        public void CopmutePower_5_raised_to_power_2()
+        {
+            Assert.AreEqual(Math.Pow(5, 2), Power.CopmutePower(5, 2));
+        }
+
+        [TestMethod()]
+        public void CopmutePower_5_raised_to_power_negative_2()
+        {
+            Assert.AreEqual(Math.Pow(5, -2), Power.CopmutePower(5, -2));
+        }
+    }
+}
